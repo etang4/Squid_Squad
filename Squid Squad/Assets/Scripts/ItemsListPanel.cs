ace9884178e18fd13f4e90d35acaaf66fb2160b4
etang4 <etang4@asu.edu>
2015-02-01 15:09:44 -0700

    ItemList Update
    Can set up the number of items to be displayed. Still needs scrollable
    menu.

---
+++ Squid Squad/Assets/Scripts/ItemsListPanel.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemsListPanel : MonoBehaviour {
+
+	public GameObject panel;
+	public GameObject[] items;
+	public int listSize;
+
+	// Use this for initialization
+	void Start () {
+		for(int i = 0; i < listSize - 1; i++){
+			GameObject pane = Instantiate(panel) as GameObject;
+			pane.transform.parent = this.transform;
+			//set new panels to correct size
+			pane.transform.localScale = new Vector3(panel.transform.localScale.x,panel.transform.localScale.y,1);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
