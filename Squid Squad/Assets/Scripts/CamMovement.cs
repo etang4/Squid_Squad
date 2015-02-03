af364e431076824a510f5a70a48b327d6356ce2a
etang4 <etang4@asu.edu>
2015-02-02 22:22:26 -0700

    Editor GUI v0.1
    Prototype of Editor GUI

---
+++ Squid Squad/Assets/Scripts/CamMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamMovement : MonoBehaviour {
+
+	public float CamSpeed = 20f;
+	public bool moveDown = false;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void MoveCameraDown(){
+		transform.Translate (0,0,-CamSpeed,Space.World);
+	}
+
+	public void MoveCameraUp(){
+		transform.Translate (0,0,CamSpeed,Space.World);
+	}
+
+	public void MoveCameraLeft(){
+		transform.Translate (-CamSpeed,0,0,Space.World);
+	}
+
+	public void MoveCameraRight(){
+		transform.Translate (CamSpeed,0,0,Space.World);
+	}
+
+	public void AdjustCamSpeed(float value){
+		CamSpeed = value;
+	}
+}
--- Squid Squad/Assets/Scripts/ItemsListPanel.cs
+++ Squid Squad/Assets/Scripts/ItemsListPanel.cs
@@ -3,17 +3,20 @@ using System.Collections;
 
 public class ItemsListPanel : MonoBehaviour {
 
-	public GameObject panel;
+	public GameObject originalButton;
 	public GameObject[] items;
 	public int listSize;
 
 	// Use this for initialization
 	void Start () {
 		for(int i = 0; i < listSize - 1; i++){
-			GameObject pane = Instantiate(panel) as GameObject;
-			pane.transform.parent = this.transform;
+			GameObject newButton = Instantiate(originalButton) as GameObject;
+			newButton.transform.parent = this.transform;
 			//set new panels to correct size
-			pane.transform.localScale = new Vector3(panel.transform.localScale.x,panel.transform.localScale.y,1);
+			Debug.Log (Screen.height);// / 15.04f
+			Debug.Log(Screen.width);// / 8.33f
+			//Resize image/button with screen height and screen width.
+			//newButton.transform.localScale = new Vector3(originalButton.transform.localScale.x,originalButton.transform.localScale.y,1);
 		}
 	}
 	
