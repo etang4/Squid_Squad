121021abbf6b849c7df65b980e087be859b35eb9
Christine <Christine.lam.1@outlook.com>
2015-02-02 23:25:07 -0700

    updates

---
+++ Assets/Collider.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+var sparkles: GameObject;
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnCollisionEnter(col: Collision) 
+{
+	//if the other object of collision is tagged as "Player"
+	if(col.gameObject.tag == "Player") 
+	{
+		//create particles
+		Instantiate(sparkles, transform.position, Quaternion.identity);
+		var flameEmitter: ParticleSystem = flame.GetComponent(ParticleSystem);
+		flameEmitter.enableEmission=true;
+
+		Destroy(gameObject);
+	}  
+}
---
+++ Assets/Scripts/TitleMenu.js
@@ -0,0 +1,33 @@
+﻿#pragma strict
+
+var customSkin: GUISkin;
+
+function OnGUI () 
+{
+	var buttonW: int = 100;
+	var buttonH: int = 50;
+	var halfScreenW: float = Screen.width/2;
+	var halfButtonW: float = buttonW/2;
+
+	//halfScreenW-halfButtonW, 160, 100, 50
+	var myRect = new Rect(halfScreenW-halfButtonW - 60, 300, buttonW, buttonH);
+	
+	var isButtonCreated : boolean = GUI.Button(myRect, "Play Game");
+	
+	var isButtonCreated2 : boolean = GUI.Button(new Rect(halfScreenW-halfButtonW -60, 380, buttonW, buttonH), "Credits");
+	
+	GUI.color = Color.white;
+	
+	if (isButtonCreated) 
+	{
+		GUI.skin = customSkin;
+		Application.LoadLevel("LevelOne");
+	}
+	
+	if (isButtonCreated2) 
+	{
+		GUI.skin = customSkin;
+		Application.LoadLevel("Credits");
+	}
+	
+}
