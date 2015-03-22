8d0ffa9aab7dc357910100ba459db2dc4564f63b
etang4 <etang4@asu.edu>
2015-03-21 23:04:36 -0700

    My Work as of last week.
    Making a local branch instead now.

--- Squid Squad/Assets/Scripts/Editor Scripts/ClickDragObjects.cs
+++ Squid Squad/Assets/Scripts/Editor Scripts/ClickDragObjects.cs
@@ -8,11 +8,13 @@ public class ClickDragObjects : MonoBehaviour {
 	private RaycastHit hit;
  	public LayerMask terrainMask;
  	private float moveSpeed;
+ 	float snapUnit;
 
  	private bool justCreated;
 
  	void Start(){
- 		moveSpeed = 4f;
+ 		snapUnit = 5f;
+ 		moveSpeed = 40f;
  		justCreated = true;
  	}
 
@@ -32,23 +34,40 @@ public class ClickDragObjects : MonoBehaviour {
  		else{
  			if(Input.GetMouseButton(0)){
  				screenPoint = Camera.main.camera.ScreenPointToRay(Input.mousePosition);
- 				if(Physics.Raycast(screenPoint, out hit, Mathf.Infinity, terrainMask)){
- 					Move(hit.point);
+ 				if(Physics.Raycast(screenPoint, out hit, Mathf.Infinity)){
+ 					if(hit.collider.gameObject == character){
+ 						if(Physics.Raycast(screenPoint, out hit, Mathf.Infinity, terrainMask)){
+				 			Move(hit.point);
+				 		}
+ 					}
  				}
  			}
  		}
  	}
 
  	void Move(Vector3 targetPos){
+ 		//Allow Rotation
+ 		Rotate();
  		Vector3 startPosition = character.transform.position;
  		Vector3 endPosition = targetPos;
+ 		character.transform.position = new Vector3(((int) (endPosition.x / snapUnit))* snapUnit,((int)(endPosition.y / snapUnit))* snapUnit,((int) (endPosition.z / snapUnit))* snapUnit);
+ 		
 
  		float t = 0.0f;
 
  		while(t < 1.0f){
  			t += Time.deltaTime * moveSpeed;
- 			transform.position = Vector3.Lerp(startPosition, endPosition, t);
+ 			//transform.position = Vector3.Lerp(startPosition, new Vector3((endPosition.x / snapUnit)* snapUnit,(endPosition.y / snapUnit)* snapUnit,(endPosition.z / snapUnit)* snapUnit), t);
  			
+ 		}
+ 	}
+
+ 	void Rotate(){
+ 		if(Input.GetKeyUp("r")){
+ 			transform.Rotate(0,90,0);
+ 		}
+ 		if(Input.GetKeyUp("w")){
+ 			transform.Rotate(0,-90,0);
  		}
  	}
 }
