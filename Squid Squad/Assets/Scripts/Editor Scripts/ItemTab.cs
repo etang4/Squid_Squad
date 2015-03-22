34213a443476d5bab60d7920bde3d2c523c61727
Christine <Christine.lam.1@outlook.com>
2015-03-21 23:35:22 -0700

    Updated with new UI scripts

--- Squid Squad/Assets/Scripts/Editor Scripts/ItemTab.cs
+++ Squid Squad/Assets/Scripts/Editor Scripts/ItemTab.cs
@@ -6,29 +6,29 @@ public class ItemTab : MonoBehaviour {
 	public GameObject itemTab;
 	public GameObject gameplayTab;
 	public GameObject chooseFPTab; //this tab will allow users to select a cart to control
-
+	
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
-
+	
 	public void ClickedItemTab(){
 		itemTab.SetActive(true);
 		gameplayTab.SetActive(false);
 		chooseFPTab.SetActive(false);
 	}
-
+	
 	public void ClickedGameplayTab(){
 		itemTab.SetActive(false);
 		gameplayTab.SetActive(true);
 		chooseFPTab.SetActive(false);
 	}
-
+	
 	public void ClickedChooseFPTab(){
 		itemTab.SetActive(false);
 		gameplayTab.SetActive(false);
